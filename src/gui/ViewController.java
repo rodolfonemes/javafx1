5f4c2fa36205a0abf48172792e412ac3aa1e6dc3
Rodolfo Nemes <tecnico.rodolfoo@gmail.com>
2022-10-15 17:18:09 -0300

    Usando TextField e Label (app para calcular soma)

--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -1,18 +1,41 @@
 package gui;
 
+import java.util.Locale;
+
 import gui.util.Alerts;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class ViewController {
 
 	@FXML
-	private Button btTest;
-	
+	private TextField txtNumber1;
+
 	@FXML
-	public void onBtTestAction() {
-		System.out.println("clique!!!!!!!!");
-		Alerts.showAlert("Alert title", null, "Hello", AlertType.WARNING);
+	private TextField txtNumber2;
+
+	@FXML
+	private Label labelResult;
+
+	@FXML
+	private Button btSoma;
+
+	@FXML
+	public void onBtSomaAction() {
+
+		Locale.setDefault(Locale.US);
+		// System.out.println("clique!!!!!!!!");
+		try {
+			double number1 = Double.parseDouble(txtNumber1.getText());
+			double number2 = Double.parseDouble(txtNumber2.getText());
+			double soma = number1 + number2;
+			Alerts.showAlert("Alert title", null, "Resultdo = " + String.format("%.2f", soma), AlertType.WARNING);
+			labelResult.setText(String.format("%.2f", soma));
+		} catch (NumberFormatException e) {
+			Alerts.showAlert("ERROR", null, e.getMessage(), AlertType.ERROR);
+		}
 	}
 }
